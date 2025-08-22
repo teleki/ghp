a47a8c5ff2628c185f7eeb51c85d498181c6d4a7
Steve Teleki <steve@teleki.net>
2025-08-22 15:55:37 -0500

    Deleted docs folder

--- docs/_next/static/chunks/15333e5ced54dbe6.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/2bf314589ff0369d.js",
-  "static/chunks/c5d2fd6bbf9b810d.js",
-  "static/chunks/turbopack-0b43318f36eddb4a.js"
-])
--- docs/_next/static/chunks/b09f16c52b51c429.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/96d2f97fb06cefa1.js",
-  "static/chunks/c5d2fd6bbf9b810d.js",
-  "static/chunks/turbopack-37b34278088f049b.js"
-])
